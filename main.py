6b49d75093b08315e9759ee657795035e39386b2
JulijaK <77327993+mintiden@users.noreply.github.com>
2022-02-01 14:06:34 +0000

    6.uzdevums

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import turtle
+import turtle, random
+krasas = ["red", "magenta", "purple",]
 Rafael = turtle.Turtle()
+Rafael.speed(0)
 Rafael.shape("turtle")
+Rafael.color("magenta")
 
 for i in range(0,12):
   Rafael.circle(100)
   Rafael.circle(100,steps=4)
+  Rafael.circle(100,steps=3)
   Rafael.right(30)
+  Rafael.color(random.choice(krasas))
